feaa55ad5ef656defd0dc98e9805d10aa6eeeff6
ivakto <ivakto11@gmail.com>
2025-10-02 21:17:51 +0300

    Implement create function to display words on click

--- 5. DOM Events/Exercise. DOM Event/01. Sections/script.js
+++ 5. DOM Events/Exercise. DOM Event/01. Sections/script.js
@@ -1,3 +1,22 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentRef = document.getElementById("content");
+   
+   if (!contentRef) {
+      return;
+   }
+
+   for (let word of words) {
+      const newDiv = document.createElement('div');
+      const newPar = document.createElement('p');
+      newPar.textContent = word;
+      newPar.style.display = 'none';
+      newDiv.appendChild(newPar);
+
+      newDiv.addEventListener('click', function() {
+         newPar.style.display = 'block';
+      });
+
+      contentRef.appendChild(newDiv);
+
+   }
 }
